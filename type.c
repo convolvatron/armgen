d46dbf66f5d542310e1de06a7dc16fc5adf89288
Eric Hoffman <yuri@bridgeport.local>
2026-02-06 10:38:39 -0800

    .

--- type.c
+++ type.c
@@ -1,16 +1,21 @@
 #include <b.h>
 
-typedef type value;
 
-type vector_of(region r, type t) {
+//typedef type value;
+
+value one = ((void *)1);
+value zero = ((void *)0);
+
+/*type vector_of(region r, type t) {
     return map(r);
 }
-
+*/
 
 vector vector_internal(region r, ...) {
-    int args = argcount(r);
-    vector v = sallocate(r, args*bitsizeof(value));
+    u64 args = argcount(r);
+    void **v = (void **)sallocate(r, args*bitsizeof(value));
     int count = 1;
+    v[count++] = (void *)args;
     valargs(r, i) v[count++] = i;
     return v;
 }
